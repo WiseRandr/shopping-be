2b2d94029050f0bb353ac2d6902997c2bc2ddc43
Wise Randr <randriamialisonwise@gmail.com>
2024-03-28 18:33:49 +0300

    cors issue fixed

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import * as express from "express";
 import { v1Routes } from "./src/routes/routes";
+import * as cors from "cors";
 
 const app = express();
 const port = 8000;
 
 async function Boot() {
   app.use(express.json());
+  app.use(cors());
   
   app.get("/", (_, res) => {
     res.send({ response: "ok", code: 200 })
